bd54d6a13feee579b976fabd1e40942b76d4a0d4
huxiaohei <huxiaoheigame@gmail.com>
2019-10-19 12:31:57 +0800

    面剔除

---
+++ OpenGL_037/src/glsl/cullFace.fs.glsl
@@ -0,0 +1,21 @@
+/*================================================================
+* Description 面剔除
+* Email huliuworld@yahoo.com
+* Created on Fri Oct 04 2019 11:48:38
+* Copyright (c) 2019 刘虎
+================================================================*/
+
+#version 410 core
+
+out vec4 fragColor;
+in vec2 texcoord;
+
+uniform sampler2D textureSampler2D;
+
+void main() {
+    vec4 color = texture(textureSampler2D, texcoord);
+    if (color.a < 0.1) {
+        discard;
+    }
+    fragColor = color;
+}
--- README.md
+++ README.md
@@ -40,3 +40,4 @@
 * [模版测试]<https://github.com/huxiaohei/OpenGL/tree/master/OpenGL_034>
 * [渲染带透明区域的图片]<https://github.com/huxiaohei/OpenGL/tree/master/OpenGL_035>
 * [渲染半透明纹理]<https://github.com/huxiaohei/OpenGL/tree/master/OpenGL_036>
+* [面剔除]<https://github.com/huxiaohei/OpenGL/tree/master/OpenGL_037>
